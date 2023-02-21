5e28e775b76ac6dd04f77b43c79bd294e4f777ab
olegm <olegm@DESKTOP-3HEOTI7>
2023-02-21 10:16:45 +0700

    Добавьте файлы проекта.

---
+++ HiWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, World!!!");
+            Console.ReadLine(); Console.Clear();
+            Console.Write("How ");
+            Console.Write("are ");
+            Console.Write("you ");
+            Console.Write("world");
+            Console.Write("?");
+            Console.ReadKey();
+        }
+    }
+}
